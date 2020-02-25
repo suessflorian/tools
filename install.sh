16a727ee693f1e47e1348739e07ef0a3ee7b7412
Florian Suess <me@floriansuess.com>
2020-02-26 12:21:21 +1300

    SSH git clone insteadOf, delete tpm clone install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,3 @@ ln -s -f ~/.dotfiles/.fzf.zsh               ~/.fzf.zsh
 ln -s -f ~/.dotfiles/.tmux.conf             ~/.tmux.conf
 ln -s -f ~/.dotfiles/alacritty.yml          ~/.config/alacritty/alacritty.yml
 ln -s -f ~/.dotfiles/init.vim               ~/.config/nvim/init.vim
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
