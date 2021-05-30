91217dec3f25235b9753f3a8f95f1e9df7a66b01
Florian Suess <floriansuess96@icloud.com>
2021-05-30 15:33:45 +1200

    Move to init.lua

---
+++ init.lua
@@ -0,0 +1,87 @@
+------------------------------------ PLUGINS ------------------------------------
+vim.cmd 'packadd paq-nvim'
+local paq = require('paq-nvim').paq
+
+paq {'savq/paq-nvim', opt=true} -- let paq manage itself
+paq {'tpope/vim-surround'}
+paq {'nvim-treesitter/nvim-treesitter'}
+paq {'tpope/vim-vinegar'}
+paq {'neovim/nvim-lspconfig'}
+paq {'junegunn/fzf', run = vim.fn['fzf#install']}
+paq {'junegunn/fzf.vim'}
+paq {'ojroques/nvim-lspfuzzy'}
+paq {'romainl/vim-cool'}
+paq {'sainnhe/sonokai'}
+
+------------------------------------- THEME -------------------------------------
+vim.cmd 'colorscheme sonokai'
+vim.cmd 'let g:sonokai_disable_italic_comment = 1'
+vim.cmd 'let g:sonokai_transparent_background = 1'
+
+------------------------------------ OPTIONS ------------------------------------
+vim.g.mapleader=" "
+
+vim.opt.expandtab=true -- spaces instead of tabs
+vim.opt.shiftwidth=2 -- size of a tab
+vim.opt.tabstop=2 -- number of spaces tab counts for
+vim.opt.shiftround=true -- round indent
+
+vim.opt.backup=false -- disable backup files
+vim.opt.swapfile=false -- no swap files
+vim.opt.autoread=true -- detect file changes outside of vim
+
+vim.opt.cursorline=true -- show cursor line
+vim.opt.inccommand="nosplit" -- show effect of command incrementally
+vim.opt.mouse="a" -- let mouse do stuff
+vim.opt.wrap=false -- disable text wrapping
+vim.opt.undofile=true -- persistant file undo's
+
+------------------------------------ MAPPINGS -----------------------------------
+vim.api.nvim_set_keymap('n', '<leader>p', ':Files<space><cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>b', ':Buffers<space><cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>f', ':Rg<space>', {noremap = true})
+
+---------------------------------- TREE SITTER ----------------------------------
+local ts = require 'nvim-treesitter.configs'
+ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
+
+------------------------------------ LS SETUP -----------------------------------
+local nvim_lsp = require('lspconfig')    
+
+-- Use an on_attach function to only map the following keys     
+-- after the language server attaches to the current buffer    
+local on_attach = function(client, bufnr)    
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end    
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end    
+
+  --Enable completion triggered by <c-x><c-o>    
+  buf_set_option('completefunc', 'v:lua.vim.lsp.omnifunc')    
+
+  -- Mappings.    
+  local opts = { noremap=true, silent=true }    
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions    
+  buf_set_keymap('n', '<C-n>,','<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<C-p>;','<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', '<C-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts) 
+  buf_set_keymap('n', 'K',     '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gt',    '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts) 
+  buf_set_keymap('n', 'gf',    '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  buf_set_keymap('n', 'ga',    '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gR',    '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', 'gr',    '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', 'go',    '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
+end    
+
+-- Use a loop to conveniently call 'setup' on multiple servers and    
+-- map buffer local keybindings when the language server attaches    
+local servers = { "gopls", "tsserver", "pyls", "graphql" }    
+for _, lsp in ipairs(servers) do    
+  nvim_lsp[lsp].setup { on_attach = on_attach }    
+end    
+
+require('lspfuzzy').setup {}  -- make the lsc use fzf instead of the quickfix list
+
+-------------------------------------- MISC -------------------------------------
+vim.cmd 'autocmd TextYankPost * lua vim.highlight.on_yank {on_visual = false}' -- highlight yanks
+vim.cmd 'autocmd FocusGained,BufEnter * checktime' -- force file change check
--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
+git clone https://github.com/savq/paq-nvim.git \
+   "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/opt/paq-nvim
+
 ln -s -f $(pwd)/gitconfig      ~/.gitconfig
 ln -s -f $(pwd)/zshrc          ~/.zshrc
-ln -s -f $(pwd)/vimrc          ~/.config/nvim/init.vim
+# ln -s -f $(pwd)/vimrc          ~/.config/nvim/init.vim
 ln -s -f $(pwd)/tmux.conf      ~/.tmux.conf
 ln -s -f $(pwd)/alacritty.yml  ~/.config/alacritty/alacritty.yml
 ln -s -f $(pwd)/urls           ~/.newsboat/urls
 ln -s -f $(pwd)/newsboat       ~/.newsboat/config
+ln -s -f $(pwd)/init.lua       ~/.config/nvim/init.lua
