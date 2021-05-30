276bec70d673f554c484f2dc3a21f6819dae4920
Florian Suess <floriansuess96@icloud.com>
2021-05-30 16:40:00 +1200

    Add gh-line, remove trailing spaces

--- init.lua
+++ init.lua
@@ -7,6 +7,7 @@ paq {'tpope/vim-surround'}
 paq {'nvim-treesitter/nvim-treesitter'}
 paq {'tpope/vim-vinegar'}
 paq {'neovim/nvim-lspconfig'}
+paq {'ruanyl/vim-gh-line'}
 paq {'junegunn/fzf', run = vim.fn['fzf#install']}
 paq {'junegunn/fzf.vim'}
 paq {'ojroques/nvim-lspfuzzy'}
@@ -25,6 +26,7 @@ vim.opt.expandtab=true -- spaces instead of tabs
 vim.opt.shiftwidth=2 -- size of a tab
 vim.opt.tabstop=2 -- number of spaces tab counts for
 vim.opt.shiftround=true -- round indent
+vim.opt.hidden=true -- allows buffer hiding rather than abandoning
 
 vim.opt.backup=false -- disable backup files
 vim.opt.swapfile=false -- no swap files
@@ -36,9 +38,13 @@ vim.opt.mouse="a" -- let mouse do stuff
 vim.opt.wrap=false -- disable text wrapping
 vim.opt.undofile=true -- persistant file undo's
 
+vim.opt.foldmethod="expr" -- treesitters determines folding
+vim.opt.foldexpr='nvim_treesitter#foldexpr()'
+vim.opt.foldlevel=99 -- open files unfolded
+
 ------------------------------------ MAPPINGS -----------------------------------
-vim.api.nvim_set_keymap('n', '<leader>p', ':Files<space><cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>b', ':Buffers<space><cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>p', ':Files<space><cr>', {noremap = true, silent=true})
+vim.api.nvim_set_keymap('n', '<leader>b', ':Buffers<space><cr>', {noremap = true, silent=true})
 vim.api.nvim_set_keymap('n', '<leader>f', ':Rg<space>', {noremap = true})
 
 ---------------------------------- TREE SITTER ----------------------------------
@@ -46,37 +52,38 @@ local ts = require 'nvim-treesitter.configs'
 ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
 
 ------------------------------------ LS SETUP -----------------------------------
-local nvim_lsp = require('lspconfig')    
+local nvim_lsp = require('lspconfig')
 
--- use an on_attach function to only map the following keys     
--- after the language server attaches to the current buffer    
-local on_attach = function(client, bufnr)    
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end    
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end    
+-- use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-  -- <c-x><c-u>    
-  buf_set_option('completefunc', 'v:lua.vim.lsp.omnifunc')    
+  -- <c-x><c-u>
+  buf_set_option('completefunc', 'v:lua.vim.lsp.omnifunc')
 
-  local opts = { noremap=true, silent=true }    
+  local opts = { noremap=true, silent=true }
 
-  -- see `:help vim.lsp.*` for documentation on any of the below functions    
+  -- see `:help vim.lsp.*` for documentation on any of the below functions
   buf_set_keymap('n', '<C-n>,','<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<C-p>;','<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', '<C-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts) 
+  buf_set_keymap('n', '<C-]>', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'K',     '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gt',    '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts) 
+  buf_set_keymap('n', 'gt',    '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', 'gi',    '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   buf_set_keymap('n', 'gf',    '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
   buf_set_keymap('n', 'ga',    '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_set_keymap('n', 'gR',    '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_set_keymap('n', 'gr',    '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   buf_set_keymap('n', 'go',    '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
-end    
+end
 
--- map buffer local keybindings when the language server attaches    
-local servers = { "gopls", "tsserver", "pyls", "graphql" }    
-for _, lsp in ipairs(servers) do    
-  nvim_lsp[lsp].setup { on_attach = on_attach }    
-end    
+-- map buffer local keybindings when the language server attaches
+local servers = { "gopls", "tsserver", "pyls", "graphql" }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup { on_attach = on_attach, indent = { enable = true } }
+end
 
 require('lspfuzzy').setup {}  -- make the lsc use fzf instead of the quickfix list
 
