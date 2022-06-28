29f1fbcd796c9b862797c2a4ae043006240810f6
Florian Suess <floriansuess96@icloud.com>
2022-06-29 09:07:54 +1200

    Great folding, dropped dead weight, tabspacing

--- init.lua
+++ init.lua
@@ -4,21 +4,21 @@ if fn.empty(fn.glob(install_path)) > 0 then
 	PACKER_BOOTSTRAP = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
 end
 
-require("packer").startup(function()
+require("packer").startup(function(use)
 	use({ "wbthomason/packer.nvim" })
-	use({ "ellisonleao/glow.nvim", branch = 'main' })
 	use({ "navarasu/onedark.nvim" })
 	use({ "akinsho/bufferline.nvim", requires = "kyazdani42/nvim-web-devicons" })
 	use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
-	use({ "google/vim-jsonnet" })
 	use({ "tpope/vim-commentary" })
 	use({ "tpope/vim-surround" })
+	use({ "tpope/vim-sleuth" })
 	use({ "williamboman/nvim-lsp-installer", requires = { "neovim/nvim-lspconfig" } })
 	use({ "ruanyl/vim-gh-line" })
+	use({ 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async' })
+	use({ "kevinhwang91/nvim-bqf" })
 	use({ "romainl/vim-cool" })
 	use({ "jiangmiao/auto-pairs" })
 	use({ "j-hui/fidget.nvim" })
-	use({ "kevinhwang91/nvim-bqf" })
 	use({ "onsails/lspkind-nvim" })
 	use({ "nvim-treesitter/nvim-treesitter", requires = { { "p00f/nvim-ts-rainbow" }, { "windwp/nvim-ts-autotag" } } })
 	use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" }, { "kyazdani42/nvim-web-devicons" } } })
@@ -33,7 +33,6 @@ require("packer").startup(function()
 		},
 	})
 	use({ "kyazdani42/nvim-tree.lua", requires = { "kyazdani42/nvim-web-devicons" } })
-	use({ "petertriho/nvim-scrollbar" })
 
 	if PACKER_BOOTSTRAP then
 		require("packer").sync()
@@ -43,7 +42,8 @@ end)
 ---- TODO:
 -- review file exploration custom mappings, align with Netrw
 -- add better hover doc rendering, no real support out there atm
--- funky over indentation issue
+-- new window behaviour in Kitty weird
+-- tree navigation
 
 -----------------------------------CORE
 local global = vim.g
@@ -52,22 +52,17 @@ global.do_filetype_lua = 1 -- use filetype.lua to detect filetype
 
 local options = vim.opt
 -- TABBING
-options.softtabstop = 2
-options.shiftwidth = 2 -- spaces per tab (when shifting), when using the >> or << commands
 options.tabstop = 2 -- spaces per tab
-options.smarttab = true -- <tab>/<BS> indent/dedent in leading whitespace
-options.autoindent = true -- maintain indent of current line
-options.expandtab = false -- don't expand tabs into spaces
 -- BACKUP
 options.backup = false -- disable backup files
 options.swapfile = false -- no swap files
 options.autoread = true -- detect file changes outside of vim
 options.undofile = true -- persistant file undo's
 -- FOLDING
-options.foldlevel = 5
-options.foldenable = false
-options.foldmethod = "expr" -- expression based folding
-options.foldexpr = "nvim_treesitter#foldexpr()" -- in particular use treesitter expressions
+options.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+options.foldcolumn = '0' -- hide for now, waiting on https://github.com/neovim/neovim/pull/17446
+options.foldlevel = 99 -- feel free to decrease the value
+options.foldenable = true
 -- APPEARANCE BEHAVIOUR
 options.termguicolors = true
 options.cursorline = true
@@ -80,10 +75,12 @@ options.clipboard = "unnamedplus" -- sync clipboard and default register
 options.laststatus = 3 -- global status line
 options.hidden = true -- allows hiding dirty buffers
 options.scrolloff = 3 -- always have lines bellow cursor line
+options.ignorecase = true -- case insensitive searching UNLESS /C or capital in search
+options.smartcase = true
 
 -- silent key binding, optionally pass additional options
 local bind = function(key, func, opts)
-	local opts = opts or {}
+	opts = opts or {}
 	opts.noremap, opts.silent = true, true
 	vim.keymap.set('n', key, func, opts)
 end
@@ -94,11 +91,11 @@ require("onedark").load()
 require("nvim-tree").setup({ git = { enable = false } })
 require("bufferline").setup()
 require("fidget").setup({ window = { blend = 0 } })
-require("scrollbar").setup()
 
 -----------------------------------GREPPING
 local telescope = require('telescope.builtin')
 bind("<leader>p", telescope.find_files)
+bind("<leader>b", telescope.buffers)
 bind("<leader>F", telescope.live_grep)
 bind("<leader>f", telescope.grep_string)
 
@@ -154,6 +151,12 @@ cmp.setup.cmdline(':', {
 -----------------------------------LSC
 local lsc = vim.lsp
 local lsp_installer = require("nvim-lsp-installer")
+local capabilities = require("cmp_nvim_lsp").update_capabilities(lsc.protocol.make_client_capabilities())
+capabilities.textDocument.foldingRange = {
+	dynamicRegistration = false,
+	lineFoldingOnly = true
+}
+
 lsp_installer.on_server_ready(function(server)
 	server:setup({
 		on_attach = function(_, bufnr)
@@ -168,7 +171,7 @@ lsp_installer.on_server_ready(function(server)
 			buffer_bind('gf', lsc.buf.formatting)
 			buffer_bind('gR', lsc.buf.rename)
 			buffer_bind('gd', lsc.buf.declaration)
-			buffer_bind('ga', vim.lsp.buf.code_action)
+			buffer_bind('ga', lsc.buf.code_action)
 
 			-- telescope wrapped LSC bindings
 			buffer_bind('<C-]>', telescope.lsp_definitions)
@@ -176,10 +179,11 @@ lsp_installer.on_server_ready(function(server)
 			buffer_bind('gr', telescope.lsp_references)
 			buffer_bind('gs', telescope.lsp_document_symbols)
 		end,
-		capabilities = require("cmp_nvim_lsp").update_capabilities(lsc.protocol.make_client_capabilities()),
+		capabilities = capabilities,
 	})
 end)
-lsc.handlers["textDocument/publishDiagnostics"] = lsc.with(lsc.diagnostic.on_publish_diagnostics, { virtual_text = false })
+lsc.handlers["textDocument/publishDiagnostics"] = lsc.with(lsc.diagnostic.on_publish_diagnostics,
+	{ virtual_text = false })
 lsc.handlers["textDocument/hover"] = lsc.with(lsc.handlers.hover, { border = "rounded" })
 
 -----------------------------------MISC
@@ -195,3 +199,4 @@ gitsigns.setup({
 		buffer_bind('[c', gitsigns.prev_hunk)
 	end
 })
+require('ufo').setup() -- code folding
